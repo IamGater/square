7efac6699b37b37e33412c3795f7da180934c423
IamGater <116513307+IamGater@users.noreply.github.com>
2023-12-19 15:58:03 +0200

    finish

--- app.js
+++ app.js
@@ -4,22 +4,31 @@ const $time = document.querySelector("#time");
 const $result = document.querySelector("#result");
 const $timeHeader = document.querySelector("#time-header");
 const $resultHeader = document.querySelector("#result-header");
+const $gameTime = document.querySelector("#game-time");
 
 let score = 0;
 let isGameStarted = false;
 
 $start.addEventListener("click", startGame);
 $game.addEventListener("click", handleBoxClick);
+$gameTime.addEventListener("input", setGameTime);
+
+function show($el) {
+  $el.classList.remove("hide");
+}
+
+function hide($el) {
+  $el.classList.add("hide");
+}
 
 function startGame() {
   score = 0;
   setGameTime();
-  $timeHeader.classList.remove("hide");
-  $resultHeader.classList.add("hide");
+  $gameTime.setAttribute("disabled", "true");
   isGameStarted = true;
   $game.classList.add("active");
-  $start.classList.add("hide");
-  
+  hide($start);
+
   const interval = setInterval(() => {
     const time = parseFloat($time.textContent);
 
@@ -37,18 +46,21 @@ function setGameScore() {
   $result.textContent = score.toString();
 }
 function setGameTime() {
-  let time = 5;
+  let time = +$gameTime.value;
   $time.textContent = time.toFixed(1);
+  show($timeHeader);
+  hide($resultHeader);
 }
 
 function endGame() {
   isGameStarted = false;
   setGameScore();
-  $start.classList.remove("hide");
+  $gameTime.removeAttribute("disabled");
+  show($start);
   $game.innerHTML = "";
   $game.style.backgroundColor = "#ccc";
-  $timeHeader.classList.add("hide");
-  $resultHeader.classList.remove("hide");
+  hide($timeHeader);
+  show($resultHeader);
 }
 
 function handleBoxClick(e) {
